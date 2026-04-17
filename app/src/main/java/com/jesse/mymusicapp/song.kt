0262e0328a2ab41dd25af033d1b4058400c05769
JesseBennett-lab <j.bennett0505@gmail.com>
2026-04-17 16:16:32 +0200

    updated variables

---
+++ app/src/main/java/com/jesse/mymusicapp/song.kt
@@ -0,0 +1,44 @@
+package com.jesse.mymusicapp
+
+import android.util.Log
+
+class song(songName:String, songArtist:String, songYear:Int, songDuration:String) {
+
+    var name :String =""
+    var artist : String =""
+    var year : Int=0
+    var duration : String =""
+
+    init{
+         name = songName
+         artist = songArtist
+         year = songYear
+         duration = songDuration
+
+
+    }
+
+    //Function to play a song-----------------------------------------------------------------------
+    fun Play(){
+        Log.v("Song", "$name is playing now")
+
+
+    }
+    //Function to pause a song-----------------------------------------------------------------------
+    fun Pause(){
+        Log.v("Song", "$name is paused now")
+
+    }
+    //Function to stop a song-----------------------------------------------------------------------
+    fun Stop(){
+        Log.v("Song", "$name is stopped now")
+
+    }
+    //Function  to favorite a song------------------------------------------------------------------
+    fun Favorite():Boolean{
+        Log.v("Song", "$name is favorited now")
+
+
+    }
+
+}
